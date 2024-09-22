2a51b07198aba0ef1b9590e593571fc2ebc9636f
pavelo8501 <pavelo8501@gmail.com>
2024-09-22 22:01:50 +0300

    after restore

---
+++ src/main/resources/static/components/js/exchangePinger.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    console.log("Script loaded");
+    if($.ajax){
+        console.log("Ajax ready");
+    }else{
+        console.log("Ajaxa ti gde??????");
+    }
+
+    $('#ping_btn').on('click', function(event) {
+
+        const fromCurrency = $('#fromCurrency').val();
+        const toCurrency = $('#toCurrency').val();
+        const amount = $('#amount').val();
+
+        const postBody = {"action":"calculate", "data" : {
+         "fromCurrency" : fromCurrency, "toCurrency" : toCurrency, "amount" : amount }
+        }
+
+        $.ajax({
+            url: '/api/rates',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(postBody),
+            success: function(response) {
+                console.log(response);
+                $('#amount_label').text(response.data.result);
+            },
+            error: function(error) {
+                console.error('Error:', error);
+            }
+        });
+    });
+});
